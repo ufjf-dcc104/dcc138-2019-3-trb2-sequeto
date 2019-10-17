284ab467cd8c1d42ae18a9460c70b33717035193
sequeto <joaopedrosequeto@gmail.com>
2019-10-17 19:53:02 -0300

    Adicionando Largura e Altura do Sprite

--- sprite.js
+++ sprite.js
@@ -1,10 +1,12 @@
 function Sprite(params = {}) {
     var exemplo = {
-        x: 0,
-        y: 0,
-        vx: 0,
-        vy: 0,
-        props: {},
+        x: 0, // Posição em x
+        y: 0, // Posição em y
+        vx: 0, // Velocidade no eixo x
+        vy: 0, // Velocidade no eixo y
+        w: 0, // Largura do Sprite
+        h: 0, // Altura do Sprite
+        props: {}, // Propriedades do Sprite
         comportar: undefined,
         scene: undefined
     }
